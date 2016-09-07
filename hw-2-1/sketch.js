d3b8d4b26dc9092d8697daa87acaf56c09a7b8c0
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-07 11:50:13 -0600

    added background color to my canvas

--- hw-2-1/sketch.js
+++ hw-2-1/sketch.js
@@ -1,7 +1,14 @@
-function setup() {
+// hoemwork 2.1
 
+// description: sketching shapes
+
+
+function setup() {
+  createCanvas(500,500);
+  background(255,255,0);
+  
 }
 
 function draw() {
-  
+
 }
