b950c9eee6652556431ed21e795857db473f576d
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-11 14:45:34 -0600

    spaces

--- basic-template/sketch.js
+++ basic-template/sketch.js
@@ -14,10 +14,13 @@ function draw() {
   ellipse(220,140,80,40);
   fill(0,0,0);
   noStroke();
+
   ellipse(250, 180, 290, 340);
   fill(250,240,230);
   noStroke();
+
   rect(175,250,150,200);
   fill(250,240,230);
+  noStroke();
 
 }
