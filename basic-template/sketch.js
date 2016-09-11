3250be6634cfe40bb1724b2d85de39e3c1d8c817
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-11 15:00:35 -0600

    changed ellipse location

--- basic-template/sketch.js
+++ basic-template/sketch.js
@@ -11,15 +11,18 @@ background(127,255,212);
 
 function draw() {
 
-  ellipse(220,180,80,40);
-  fill(256,256,256);
   ellipse(250, 300, 290, 340);
   fill(250,240,230);
   noStroke();
+
   rect(175,250,150,200);
   fill(250,240,230);
   noStroke();
-  ellipse(220,180,80,40);
+
+  ellipse(horizontalCenter-10,180,80,40);
+  fill(256,256,256);
+
+  ellipse(horizontalCenter,180,80,40);
   fill(256,256,256);
 
 }
