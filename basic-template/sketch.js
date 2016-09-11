256c4dc32bf977158a43eea67425d0da05732ab0
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-11 14:55:32 -0600

    added ellipse and changed colors and took away strokes

--- basic-template/sketch.js
+++ basic-template/sketch.js
@@ -11,14 +11,15 @@ background(127,255,212);
 
 function draw() {
 
-  ellipse(220,140,80,40);
-  fill(250,240,230);
-  noStroke();
+  ellipse(220,180,80,40);
+  fill(256,256,256);
   ellipse(250, 300, 290, 340);
   fill(250,240,230);
   noStroke();
   rect(175,250,150,200);
   fill(250,240,230);
   noStroke();
+  ellipse(220,180,80,40);
+  fill(256,256,256);
 
 }
