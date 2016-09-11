02835767c8852c13ff4f67f69e19c203a2289a25
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-11 14:49:25 -0600

    color changes

--- basic-template/sketch.js
+++ basic-template/sketch.js
@@ -12,13 +12,11 @@ background(127,255,212);
 function draw() {
 
   ellipse(220,140,80,40);
-  fill(0,0,0);
+  fill(250,240,230);
   noStroke();
-
   ellipse(250, 300, 290, 340);
   fill(250,240,230);
   noStroke();
-
   rect(175,250,150,200);
   fill(250,240,230);
   noStroke();
