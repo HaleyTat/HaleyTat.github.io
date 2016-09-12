b7cca64ef2c9be5fb1fda9cc852ce42fe317d94e
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-11 22:04:03 -0600

    spaced out facial features

--- basic-template/sketch.js
+++ basic-template/sketch.js
@@ -19,24 +19,30 @@ function draw() {
   noStroke();
   fill(250,240,230);
   ellipse(250, 200, 290, 340);
+
   //neck
   noStroke();
   fill(250,240,230);
   rect(175,250,150,200);
+
   //eye 1
   fill(256,256,256);
   ellipse(horizontalCenter-10,180,80,40);
+
   //eye 2
   fill(256,256,256);
   ellipse(horizontalCenter,180,80,40);
+
   //sides of nose
   fill(256,256,256);
   ellipse(270,250,50,50);
   ellipse(230,250,50,50);
   ellipse(250,250,50,60);
+
   //middle of nose
   fill(256,256,256);
   rect(230,270,60,90);
+  
   //mouth
   fill(256,256,256);
   ellipse(250,170,90,30);
