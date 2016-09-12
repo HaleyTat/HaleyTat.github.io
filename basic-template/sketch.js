6c8003dd18fd9f5b3b3e9c0285a6159803515eba
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-11 21:52:40 -0600

    notes on code items

--- basic-template/sketch.js
+++ basic-template/sketch.js
@@ -15,22 +15,23 @@ background(127,255,212);
 }
 
 function draw() {
+  //head
+  noStroke();
+  fill(250,240,230);
+  ellipse(250, 200, 290, 340);
+  //neck
+  noStroke();
+  fill(250,240,230);
+  rect(175,250,150,200);
+  //eye 1
+  fill(256,256,256);
+  ellipse(horizontalCenter-10,180,80,40);
+  //eye 2
+  fill(256,256,256);
+  ellipse(horizontalCenter,180,80,40);
+  //sides of nose
   fill(256,256,256);
   ellipse(horizontalCenter,250,50,50);
   ellipse(horizontalCenter-10,250,50,50);
 
-  noStroke();
-  fill(250,240,230);
-  ellipse(250, 200, 290, 340);
-
-  noStroke();
-  fill(250,240,230);
-  rect(175,250,150,200);
-
-  fill(256,256,256);
-  ellipse(horizontalCenter-10,180,80,40);
-
-  fill(256,256,256);
-  ellipse(horizontalCenter,180,80,40);
-  
 }
