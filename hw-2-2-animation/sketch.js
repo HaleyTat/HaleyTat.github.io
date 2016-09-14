5601ccfd09ff88a4415fa8b7f9fc9352011640f2
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-14 16:46:08 -0600

    added arc with mouseY

--- hw-2-2-animation/sketch.js
+++ hw-2-2-animation/sketch.js
@@ -15,9 +15,13 @@ createCanvas(500,500);
 
 function draw() {
   background(255,182,193);
-  
+
   noStroke();
   fill(135,206,235);
   rect(mouseX,150,80,40);
+
+  noStroke();
+  fill(135,206,235);
+  arc(200,400,50,50,90,mouseY,OPEN);
 
 }
