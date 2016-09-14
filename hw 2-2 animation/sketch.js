79143bd47e4425c98593b2f207ab766d7039bf97
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-14 10:38:14 -0600

    added image

---
+++ hw 2-2 animation/sketch.js
@@ -0,0 +1,20 @@
+//hoemwork 2.1
+
+//description: self-portrait
+
+function preload() {
+  img = loadImage(,"gifcombo2");
+
+}
+
+function setup() {
+
+// createCanvas(500,500);
+}
+
+function draw() {
+
+
+
+
+}
