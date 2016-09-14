68a416b9fb0d65dd3334911fc6b79fde7c3844dc
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-14 10:53:17 -0600

    deleted folder and replaced sketch in right folder

--- hw-2-2-animation/sketch.js
+++ hw-2-2-animation/sketch.js
@@ -2,19 +2,22 @@
 
 //description: self-portrait
 
-function preload() {
-  img = loadImage(,"gifcombo2");
-  
-}
+ // function preload() {
+ // img = loadImage("gifcombo2");
+
+// }
 
 function setup() {
 
 createCanvas(500,500);
+background(255,182,193);
+
 }
 
 function draw() {
-
-
+  noStroke();
+  fill(135,206,235);
+  rect(200,150,80,40);
 
 
 }
