408b4761242fd076fd7b25d58a242bd9070931f7
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-15 15:41:21 -0600

    added growing ellipse from center

--- personal-work/sketch.js
+++ personal-work/sketch.js
@@ -5,6 +5,7 @@
 var horizontalCenter=400;
 var circleY=0;
 var circleYY=0;
+var circleG=0
 
 function setup() {
 
@@ -24,9 +25,17 @@ function draw() {
 
  noStroke();
  fill(255,255,0);
- ellipse(600,circleYY,100,100);
+ ellipse(700,circleYY,100,100);
 
+ noStroke();
+ fill(0,191,255);
+ ellipse(horizontalCenter,horizontalCenter,circleG,circleG);
+
+// comes down
 circleY=circleY+1;
-
+// comes down
 circleYY=circleYY+1;
+// grows from center
+circleG=circleG+5;
+
 }
