3f2250189b6d0a3554e773a38d400e33e6aa6143
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-15 15:36:47 -0600

    added another ellipse

--- personal-work/sketch.js
+++ personal-work/sketch.js
@@ -9,10 +9,11 @@ var circleYY=0;
 function setup() {
 
 createCanvas(800,800);
-background(255,248,220);
 }
 
 function draw() {
+
+ background(255,248,220);
  noStroke();
  fill(255,255,0);
  ellipse(horizontalCenter,circleY,100,100);
@@ -20,6 +21,10 @@ function draw() {
  noStroke();
  fill(255,255,0);
  ellipse(100,circleYY,100,100);
+
+ noStroke();
+ fill(255,255,0);
+ ellipse(600,circleYY,100,100);
 
 circleY=circleY+1;
 
