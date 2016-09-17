3ffc77578250815ddafd1c1833fadc795ddf2747
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-17 15:08:10 -0600

    changed all and added line

--- student-juried-show/sketch.js
+++ student-juried-show/sketch.js
@@ -5,21 +5,12 @@
 function setup() {
 
 createCanvas(800,800);
-background(255,248,220);
+background(135,206,250);
 }
 
 function draw() {
 noStroke();
-fill(135,206,250);
-rect(130,300,350,400);
-
-noStroke();
-fill(240,230,140);
-arc(400,350,550,550,130,360);
-
-
-noStroke();
-fill(255,255,255);
-ellipse(400,300,30,70);
+fill(255,215,0);
+line(0,300,800,300);
 
 }
