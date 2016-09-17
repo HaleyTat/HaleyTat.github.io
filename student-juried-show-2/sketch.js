0aa9f6e45acf9bdedf25dcea146860a9afb35036
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-17 16:23:50 -0600

    17 lines

--- student-juried-show-2/sketch.js
+++ student-juried-show-2/sketch.js
@@ -12,48 +12,48 @@ function draw() {
 // line 1
   stroke(255,250,205);
   line(0,200,800,200);
-// line 2;
-stroke(255,250,205);
-line(0,225,800,225);
-// line 3
+// line 2
   stroke(255,250,205);
   line(0,250,800,250);
-// line 4
+// line 3
 stroke(255,250,205);
 line(0,275,800,275);
-// line 5
+// line 4
 stroke(255,250,205);
 line(0,300,800,300);
-// line 6
-stroke(255,250,205);
-line(0,312,800,312);
-// line 7
+// line 5
 stroke(255,250,205);
 line(0,325,800,325);
-// line 8
+// line 6
 stroke(255,250,205);
 line(0,337,800,337);
-// line 9
+// line 7
 stroke(255,250,205);
 line(0,350,800,350);
-// line 10
+// line 8
 stroke(255,250,205);
 line(0,362,800,362);
-// line 11
+// line 9
 stroke(255,250,205);
 line(0,375,800,375);
-// line 12
+// line 10
 stroke(255,250,205);
 line(0,387,800,387);
-// line 13
+// line 11
 stroke(255,250,205);
 line(0,393,800,393);
-// line 14
+// line 12
 stroke(255,250,205);
 line(0,400,800,400);
-// line 15
+// line 13
+stroke(255,250,205);
+line(0,412,800,412);
+// line 14
 stroke(255,250,205);
 line(0,425,800,425);
+// line 15
+stroke(255,250,205);
+line(0,437,800,437);
 // line 16
 stroke(255,250,205);
 line(0,450,800,450);
