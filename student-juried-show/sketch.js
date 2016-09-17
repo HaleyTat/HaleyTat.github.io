69c1b5ae9a8047257256e32c6c496f4df9a56f7e
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-17 16:08:28 -0600

    changed stroke color and bg

--- student-juried-show/sketch.js
+++ student-juried-show/sketch.js
@@ -5,21 +5,21 @@
 function setup() {
 
 createCanvas(800,800);
-background(176,224,230);
+background(135,206,250);
 }
 
 function draw() {
 // line 1
-  stroke(255,215,0);
+  stroke(255,250,205);
   line(0,200,800,200);
 // line 2;
-stroke(255,215,0);
+stroke(255,250,205);
 line(0,225,800,225);
 // line 3
-  stroke(255,215,0);
+  stroke(255,250,205);
   line(0,250,800,250);
 // line 4
-stroke(255,215,0);
+stroke(255,250,205);
 line(0,275,800,275);
 // line 5
 stroke(255,215,0);
@@ -49,18 +49,18 @@ line(0,369,800,369);
 stroke(255,215,0);
 line(0,375,800,375);
 // line 14
-stroke(255,215,0);
+stroke(255,250,205);
 line(0,387,800,387);
 // line 15
-stroke(255,215,0);
+stroke(255,250,205);
 line(0,400,800,400);
 // line 16
-stroke(255,215,0);
+stroke(255,250,205);
 line(0,425,800,425);
 // line 17
-stroke(255,215,0);
+stroke(255,250,205);
 line(0,450,800,450);
 // line 18
-stroke(255,215,0);
+stroke(255,250,205);
 line(0,475,800,475);
 }
