99eff91764389353c7eb29372383b8a8d7a69b48
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-19 13:09:03 -0600

    changed bg color and shoes from ellipses to rect

--- hw-2-2-animation/sketch.js
+++ hw-2-2-animation/sketch.js
@@ -16,28 +16,28 @@ var spot={
 function setup() {
 
 createCanvas(500,500);
-fill(0);
-ellipse(mouseX,200,40,40);
 }
 
 function draw() {
   // screen changes color from right to left
   var yellow=map(mouseX,0,500,0,255);
-  background(0,grn,bleColor);
+  background(0,0,bleColor);
 // rect side to side
   noStroke();
   fill(135,206,235);
   rect(mouseX,150,80,40);
+  rect(mouseX,250,80,40);
+  rect(mouseX,350,80,40);
 // circle up and down
   noStroke();
   fill(135,206,235);
-  ellipse(250,mouseY,50,50);
+  rect(100,mouseY,10,150);
 // moving circles
 frameRate(20);
   spot.x=random(0,width);
   spot.y=random(0,height);
   fill(135,206,235,100);
-  ellipse(spot.x,spot.y,30,30);
+  rect(spot.x,spot.y,60,30);
 
   // noStroke();
   // fill((135,206,235);
