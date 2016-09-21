815b6ed59e6984dfc5db4d0656ed66d06c5bd006
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-21 10:20:59 -0600

    practice with conditional statement in class

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,6 +1,6 @@
-// student juried show
+// hw-3
 
-// minimalist code artwork 2
+// conditional statement hw
 
 function setup() {
 
@@ -9,59 +9,11 @@ background(135,206,250);
 }
 
 function draw() {
-  
-// line 1
-  stroke(255,250,205);
-  line(0,200,800,200);
-// line 2
-  stroke(255,250,205);
-  line(0,250,800,250);
-// line 3
-stroke(255,250,205);
-line(0,275,800,275);
-// line 4
-stroke(255,250,205);
-line(0,300,800,300);
-// line 5
-stroke(255,250,205);
-line(0,325,800,325);
-// line 6
-stroke(255,250,205);
-line(0,337,800,337);
-// line 7
-stroke(255,250,205);
-line(0,350,800,350);
-// line 8
-stroke(255,250,205);
-line(0,362,800,362);
-// line 9
-stroke(255,250,205);
-line(0,375,800,375);
-// line 10
-stroke(255,250,205);
-line(0,387,800,387);
-// line 11
-stroke(255,250,205);
-line(0,393,800,393);
-// line 12
-stroke(255,250,205);
-line(0,400,800,400);
-// line 13
-stroke(255,250,205);
-line(0,406,800,406);
-// line 14
-stroke(255,250,205);
-line(0,412,800,412);
-// line 15
-stroke(255,250,205);
-line(0,425,800,425);
-// line 16
-stroke(255,250,205);
-line(0,437,800,437);
-// line 17
-stroke(255,250,205);
-line(0,450,800,450);
-// line 18
-stroke(255,250,205);
-line(0,475,800,475);
+  textSize(40);
+if (0=="0") {
+  text(100,100,"true")
+} else {
+  text("false",100,100);
+}
+
 }
