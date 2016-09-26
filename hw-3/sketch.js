da9e635adcc76a685f71de1ee9690f26094b9b41
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-26 13:20:52 -0600

    added conditional statement

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -9,11 +9,15 @@ background(135,206,250);
 }
 
 function draw() {
-  textSize(40);
-if (0=="0") {
-  text(100,100,"true")
-} else {
-  text("false",100,100);
-}
-
+  // textSize(40);
+// if (0=="0") {
+  // text(100,100,"true")
+// } else {
+  // text("false",100,100);
+// }
+  if (mouseY>=400) {
+    fill(255,255,0);
+  } else {
+    fill(255,0,255,100);
+  }
 }
