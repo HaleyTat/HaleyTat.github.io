ceead3e59ca92c159a77d870e9a4d2f3192655e6
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-26 22:49:58 -0600

    made comments

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -2,7 +2,6 @@
 
 // conditional statement hw
 var circleMove=0;
-// var speed=3;
 
 function setup() {
 createCanvas(800,800);
@@ -10,22 +9,13 @@ createCanvas(800,800);
 
 function draw() {
   background(135,206,250);
+  // moving retcangle fom left to right
    fill(0,0,255);
    noStroke();
    rect(circleMove,300,250,150);
    circleMove++;
-// noStroke();
-// fill(0);
-// ellipse(300,200,100,100);
 
-// if (x>width || x<0) {
-  // speed=speed*-1;
-// }
-// x=x+speed;
- // else {
-  // ellipse(x,b,b,d)
-// }
-
+// button which changes from black to white
  if (mouseY>=300) {
     fill(0);
   } else {
@@ -33,5 +23,15 @@ function draw() {
  }
  rect(300,300,250,150);
 }
-// else if (mousePressed()) {
-  //  rect(begin,100,250,175);
+//Example from Daniel Shiffman and still so confused on bouncing ball
+
+// noStroke();
+// fill(0);
+// ellipse(300,200,100,100);
+// if (x>width || x<0) {
+  // speed=speed*-1;
+// }
+// x=x+speed;
+ // else {
+  // ellipse(x,b,b,d)
+// }
