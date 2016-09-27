55d6b5ace4fe7091eb8d19e6768eda44ab464095
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-26 18:12:28 -0600

    commented out else if

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,8 +1,8 @@
 // hw-3
 
 // conditional statement hw
-  var begin=0;
-  begin=begin+10;
+  // var begin=0;
+  // begin=begin+10;
 
 function setup() {
 
@@ -11,11 +11,11 @@ background(135,206,250);
 }
 
 function draw() {
-  rect(300,300,250,175);
   if (mouseY>=400) {
     fill(0,0,255);
-  } else if (mousePressed()) {
-     rect(begin,100,250,175);
+  }
+  // else if (mousePressed()) {
+    //  rect(begin,100,250,175);
   }else {
     fill(0,255,255,100);
   }
