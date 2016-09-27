9213430673cc71d79e313f1bf7c156e07284b778
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-26 19:12:08 -0600

    trying to add motion

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,8 +1,6 @@
 // hw-3
 
 // conditional statement hw
-  // var begin=0;
-  // begin=begin+10;
 
 function setup() {
 
@@ -11,14 +9,15 @@ background(135,206,250);
 }
 
 function draw() {
-if (mouseY>=300) {
-   fill(0);
-  //  rect(300,200,200,150);
- } else {
-  fill(255);
-  // rect(300,200,200,150);
-}
-rect(300,300,250,150);
+  var strt=0;
+  strt=strt+10;
+  rect(strt,300,250,150);
+// if (mouseY>=300) {
+  //  fill(0);
+ // } else {
+  // fill(255);
+// }
+// rect(300,300,250,150);
 }
 // else if (mousePressed()) {
   //  rect(begin,100,250,175);
