791f2f99224e1cf426d74b58975a06c13a76cfcc
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-26 18:09:31 -0600

    moved around and added else if

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -11,20 +11,12 @@ background(135,206,250);
 }
 
 function draw() {
-  // textSize(40);
-// if (0=="0") {
-  // text(100,100,"true")
-// } else {
-  // text("false",100,100);
-// }
   if (mouseY>=400) {
     fill(0,0,255);
-  } else {
+  } else if (mousePressed()) {
+     rect(begin,100,250,175);
+  }else {
     fill(0,255,255,100);
   }
   rect(300,300,250,175);
-   if (mousePressed()) {
-    rect(begin,100,250,175);
-   }
-  // rect(300,300,250,175);
 }
