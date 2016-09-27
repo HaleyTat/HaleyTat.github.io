d81126da6fd3ff11d68dbd09699f862a1802bd7c
HaleyTat <haley.tatalovich@umontana.edu>
2016-09-26 19:04:58 -0600

    added ellipse with mouse pressed

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -11,11 +11,11 @@ background(135,206,250);
 }
 
 function draw() {
-  if (mouseY>=300) {
-    fill(0);
-    rect(300,200,200,150);
- } else if (mousePressed()) {
-   fill(255,0,200);
+if (mousePressed()) {
+  fill(0);
+  ellipse(100,300,80,80);
+ } else if (mouseY>=300) {
+   fill(0);
    rect(300,200,200,150);
  } else {
   fill(255);
