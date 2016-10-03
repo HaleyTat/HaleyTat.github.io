f27271e8aede520867bf392d79e2588acc841ceb
HaleyTat <haley.tatalovich@umontana.edu>
2016-10-03 11:51:12 -0600

    replaced object and took out other object

--- hw-4-minimalism/sketch.js
+++ hw-4-minimalism/sketch.js
@@ -1,15 +1,13 @@
 // homework 4.2
 
 // minimalism hw
-
-// var objectSpot={
-// x:10,
-// y:20,
-// z:50,
-// };
-
+ var objectSpot={
+ x:10,
+ y:20,
+};
 function setup() {
   createCanvas(600,600);
+  objectSpot.x=random(20);
 }
 
 function draw() {
@@ -17,14 +15,4 @@ background(255);
 fill(0);
 ellipse(200,400,80,80);
 
-if (mouseIsPressed) {
-fill(255,0,255);
-if (mouseX<=200) {
-  fill(200,0,0);
-} else {
-  fill(250,240,230);
-}
-ellipse(100,300,150,150);
-
-}
 }
