50a550f29bacc8ce456530e6d303788a08881ebd
HaleyTat <haley.tatalovich@umontana.edu>
2016-10-13 14:43:02 -0600

    hopefully code should work after fixing syntax and removing blackSquare sketch link in index

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,16 +1,40 @@
 // homework 4.2
 
 // minimalism hw
-   var array=[200,300],
+  var blocks=[];
 
   function setup() {
     createCanvas(600,400);
-    background(255);
-    for (var i = 0; i < array.length; i++) {
-      
+    for (var i = 0; i < 10; i++) {
+      blocks[i]= new Block();
     }
-  }
+}
 
   function draw() {
-  objectSquare.display();
+  background(255);
+  for (var i = 0; i < blocks.length; i++) {
+    blocks[i].grow();
+    blocks[i].display();
+  }
+}
+function Block() {
+   x=200;
+   y=200;
+   sideS=40;
+   topS=40;
+// black block
+   this.display= function() {
+    noStroke();
+    fill(0);
+    rect(this.x,this.y,this.sideS,this.topS);
+   };
+// allows blocks to grow when mouse is pressed
+   this.grow= function() {
+     if (mouseIsPressed) {
+       this.sideS=this.sideS+1;
+       this.topS=this.topS+1;
+     }
+// object its referring to
+    this.display();
+    };
 }
