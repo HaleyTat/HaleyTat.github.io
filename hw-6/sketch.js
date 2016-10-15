5c15ceb4a015d2e8e5d7559479f316bd6501feaf
HaleyTat <haley.tatalovich@umontana.edu>
2016-10-15 12:41:27 -0600

    attempting to create Pacman function

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,28 +1,25 @@
 // homework 4.2
 
 // minimalism hw
- var objectSpot={
- x:10,
- y:20,
- z:10,
-};
+ function Pacman (red,green,blue) {
+
+    x=mouseX;
+    y=mouseY;
+    size=60;
+    dir=1;
+    this.r=red;
+    this.b=blue;
+    this.g=green;
+
+}
+
 function setup() {
   createCanvas(600,600);
-  frameRate(5);
-  background(255);
+
 }
 
 function draw() {
-objectSpot.x=random(width);
-objectSpot.y=random(height);
-objectSpot.z=random(255);
+background(0);
 
-// if (objectSpot.x>=400 && objectSpot.y <= 300) {
- // fill(0,0,255);
- // }
-
-noStroke();
-fill(0,255,0,objectSpot.z);
-ellipse(objectSpot.x,objectSpot.y,40,40);
 
 }
