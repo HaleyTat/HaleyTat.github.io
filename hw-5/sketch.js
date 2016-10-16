69684666736c892cc64bce7cece11d626cf487cc
HaleyTat <haley.tatalovich@umontana.edu>
2016-10-16 16:30:01 -0600

    attempt at making all shapes

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -9,8 +9,8 @@ function setup() {
 createCanvas(600,400);
 
   for (var i = 0; i < 10; i++) {
-    var x=width/2;
-    var y=height/2;
+    var x=random(0,width);
+    var y=random(0,height);
     blocks.push(new Block(x,y));
     }
 }
