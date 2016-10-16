3b7ffa0f6e5a69cb051e295215a1b44c62513ca5
HaleyTat <haley.tatalovich@umontana.edu>
2016-10-16 16:41:57 -0600

    changed order back bc it looks cooler

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -11,8 +11,8 @@ createCanvas(600,400);
   for (var i = 0; i < 100; i++) {
     var x=random(0,width);
     var y=random(0,height);
-    whiteblocks.push(new WhiteBlock(x,y));
     blocks.push(new Block(x,y));
+    whiteblocks.push(new WhiteBlock(x,y));
     }
 }
 
@@ -22,11 +22,11 @@ background(255);
 
   for (var i = 0; i < blocks.length; i++) {
 
-    whiteblocks[i].display();
-    whiteblocks[i].sizeup();
+
     blocks[i].display();
     blocks[i].grow();
-
+    whiteblocks[i].display();
+    whiteblocks[i].sizeup();
 
       }
 
