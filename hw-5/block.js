4e0369be5b5b8ba98a51b98f722be7bf80c3d9d2
HaleyTat <haley.tatalovich@umontana.edu>
2016-10-18 16:25:14 -0600

    added README.md

--- hw-5/block.js
+++ hw-5/block.js
@@ -3,51 +3,52 @@
 // minimalism hw
 
 function Block(x,y) {
-   this.x=x;
-   this.y=y;
-   this.size=40;
-   this.col=color(0);
+  this.x=x;
+  this.y=y;
+  this.size=40;
+  this.col=color(0);
 
 
-// black block
-this.display= function() {
+  // black block
+  this.display= function() {
 
     fill(this.col);
     rect(this.x,this.y,this.size,this.size);
-   };
+  };
 
-// this.clicked= function() {
-//
-//        this.col=color(250,240,230);
-// };
+  // this.clicked= function() {
+  //
+  //        this.col=color(250,240,230);
+  // };
 
-// grow function
-this.grow= function() {
+  // grow function
+  this.grow= function() {
 
-  this.size=this.size+2;
+    this.size=this.size+5;
 
-     };
+  };
 }
 
-  function WhiteBlock(x,y) {
-      this.x=x;
-      this.y=y;
-      this.size=10;
+function WhiteBlock(x,y) {
+  this.x=x;
+  this.y=y;
+  this.size=1;
 
-      this.display= function() {
+  this.display= function() {
 
-        fill(255);
-        rect(this.x,this.y,this.size,this.size);
+    noStroke();
+    fill(255);
+    rect(this.x,this.y,this.size,this.size);
 
   };
   // this.clicked= function() {
   //
   //        this.col=color(250,240,230);
 
-    this.sizeup= function() {
+  this.sizeup= function() {
 
-        this.size=this.size+1;
+    this.size=this.size+2;
 
-};
+  };
 
 }
--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -6,14 +6,14 @@ var whiteblocks=[];
 
 
 function setup() {
-createCanvas(600,400);
+  createCanvas(600,400);
 
   for (var i = 0; i < 40; i++) {
     var x=random(0,width);
     var y=random(0,height);
     blocks.push(new Block(x,y));
     whiteblocks.push(new WhiteBlock(x,y));
-    }
+  }
 }
 
 // function mousePressed() {
@@ -26,27 +26,35 @@ createCanvas(600,400);
 
 function draw() {
 
-background(255);
+  background(255);
 
-// if (mousePressed()) {
-//   fill(255);
-//   rect(this.posX,this.posY,this.size,this.size);
+  // if (mousePressed()) {
+  //   fill(255);
+  //   rect(this.posX,this.posY,this.size,this.size);
 
-  for (var i = 0; i < blocks.length; i++) {
 
-    blocks[i].display();
-    blocks[i].grow();
+  if (mouseIsPressed) {
+    for (var j = 0; j < blocks.length; j++) {
+      blocks[j].grow();
+      whiteblocks[j].sizeup();
+
+    }
+  }
+
+
+  for (var m = 0; m < whiteblocks.length; m++) {
+    blocks[m].display();
+    whiteblocks[m].display();
+  }
+
+  // for (var i = 0; i < blocks.length; i++) {
+  //   blocks[i].display();
+    // blocks[i].grow();
     // whiteblocks[i].display();
     // whiteblocks[i].sizeup();
+  // }
 
 
-}
-
-for (var m = 0; m < whiteblocks.length; m++) {
-  whiteblocks[m].display();
-  whiteblocks[m].sizeup();
-}
-
-      // }
+  // }
 
 }
