affe049a12a0163388c0542198d9c2c43891720f
HaleyTat <haley.tatalovich@umontana.edu>
2016-10-18 18:44:52 -0600

    changed and commented out to figure out bug

--- hw-6/pacman.js
+++ hw-6/pacman.js
@@ -3,40 +3,39 @@
 // pacman hw
 
 
- function Pacman () {
+ function Pacman() {
 
-    this.x=350;
-    this.y=300;
-    this.posXX=200;
-    this.posYY=350;
+    this.x=x;
+    this.y=y;
+    // this.posXX=200;
+    // this.posYY=350;
     this.size=60;
-    dir=1;
-    this.posX=100;
-    this.posY=200;
-    this.r=red;
-    this.b=blue;
-    this.g=green;
+    // dir=1;
+    // this.posX=100;
+    // this.posY=200;
+    // this.r=red;
+    // this.b=blue;
+    // this.g=green;
 
-}
-
-this.displayPac1= function() {
+this.display= function() {
 
     // translate(this.x,this.y);
 
 // first pacman
-    fill(255);
+    fill(255,255,0);
     ellipse(this.x,this.y,this.size,this.size);
 };
-
-this.displayPac2= function() {
-// second pacman
-    fill(255);
-    ellipse(this.posX,this.posY,this.size,this.size);
-
-};
-
-this.displayPac3= function() {
-  // third pacman
-      fill(255);
-      ellipse(this.posX,this.posY,this.size,this.size);
-};
+}
+//
+// this.displayPac2= function() {
+// // second pacman
+//     fill(255);
+//     ellipse(this.posX,this.posY,this.size,this.size);
+//
+// };
+//
+// this.displayPac3= function() {
+//   // third pacman
+//       fill(255);
+//       ellipse(this.posX,this.posY,this.size,this.size);
+// };
--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -2,17 +2,32 @@
 
 // pacman hw
 
+var pacman= [];
+
 
 function setup() {
+
   createCanvas(600,600);
-  background(0);
+
+  for (var i = 0; i < 10; i++) {
+    var x= random(width);
+    var y= random(height);
+    pacman.push= (new Pacman(x,y));
+  }
+
 }
 
 function draw() {
 
-Pacman.displayPac1();
-Pacman.displayPac2();
-Pacman.displayPac3();
+background(0);
+
+for (var i = 0; i < pacman.length; i++) {
+  pacman[i].displayPac1();
+}
+
+// Pacman.displayPac1();
+// Pacman.displayPac2();
+// Pacman.displayPac3();
 
 
 }
