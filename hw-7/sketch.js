7a6ecf6bccfa7c4f1909c9036d42acbe4428f899
HaleyTat <haley.tatalovich@umontana.edu>
2016-11-06 16:48:38 -0700

    changed file and saved info in index and saved info in sketch

---
+++ hw-7/sketch.js
@@ -0,0 +1,24 @@
+// homework 7.2
+
+// pacman hw
+
+var h1;
+
+
+function setup() {
+
+  createCanvas(200,200);
+  h1=createElement('h1', 'This data reflects US populations');
+}
+
+function mousePressed() {
+
+  createP('This data above are about my favorite cities');
+
+}
+
+function draw() {
+
+
+
+}
