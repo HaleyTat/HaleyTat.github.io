b6d14e08598ab358885d722d54a1bda295e7e8d4
HaleyTat <haley.tatalovich@umontana.edu>
2016-11-08 10:30:14 -0700

    changed ' to "

--- hw-7/sketch.js
+++ hw-7/sketch.js
@@ -6,14 +6,14 @@
 
 function setup() {
   createCanvas(800,400);
-
+  createP("California, Nevada, Washington, Montana");
 }
 
 function draw() {
 
 background(255,240,230);
 
-createP('California, Nevada, Washington, Montana');
+// createP("California, Nevada, Washington, Montana");
 // textSize(36);
 // text('California, Nevada, Washington, Montana',0,100,windowWidth,100);
 
