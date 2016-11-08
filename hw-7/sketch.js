6739ef09fb27f3b3fdc08ce5757237857f09f100
HaleyTat <haley.tatalovich@umontana.edu>
2016-11-08 10:23:57 -0700

    attempt at createP again

--- hw-7/sketch.js
+++ hw-7/sketch.js
@@ -13,8 +13,8 @@ function draw() {
 
 background(255,240,230);
 
-// createP('California,Nevada,Washington,Montana');
-textSize(36);
-text('California,Nevada,Washington,Montana',0,100,windowWidth,100);
+createP('California, Nevada, Washington, Montana');
+// textSize(36);
+// text('California, Nevada, Washington, Montana',0,100,windowWidth,100);
 
 }
